6a39fef93a873562db1a5593070a1218e1a7bb4a
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-01-05 13:47:16 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,22 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Standard usage (Auto-detects i18n/request.ts)
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  reactCompiler: true, // <--- Moved out of 'experimental'
+    reactCompiler: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      // --- ADD THIS BLOCK ---
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com', // <--- YouTube Thumbnails live here
+      },
+      // ----------------------
     ],
   },
 };
